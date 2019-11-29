74d5e4baaa1ec758bdd811ca070e5ebc890a1191
Hydra <ryanandrews135@gmail.com>
2019-11-29 16:04:41 -0700

    Fix party pets

--- vUI/Elements/Defaults/Defaults.lua
+++ vUI/Elements/Defaults/Defaults.lua
@@ -305,8 +305,8 @@ Defaults["party-power-height"] = 6
 Defaults["party-show-debuffs"] = true
 Defaults["party-pets-enable"] = true
 Defaults["party-pets-width"] = 76
-Defaults["party-pets-health-height"] = 0 -- NYI
-Defaults["party-pets-power-height"] = 22
+Defaults["party-pets-health-height"] = 22
+Defaults["party-pets-power-height"] = 0 -- NYI
 
 Defaults["raid-enable"] = true
 Defaults["raid-width"] = 90
