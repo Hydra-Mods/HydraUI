cc140dd2de8fc9fd8caa77a4d1a74ddb2dcf2630
Hydra <ryanandrews135@gmail.com>
2020-03-28 23:53:44 -0600

    Why so many folders?

---
+++ vUI/Elements/DruidMana.lua
@@ -0,0 +1,113 @@
+local vUI, GUI, Language, Media, Settings, Defaults = select(2, ...):get()
+
+if (vUI.UserClass ~= "DRUID") then
+	return
+end
+
+local floor = floor
+local format = format
+
+local UnitPower = UnitPower
+local UnitPowerMax = UnitPowerMax
+local UnitPowerType = UnitPowerType
+
+local DruidMana = vUI:NewModule("Druid Mana")
+local ManaID = Enum.PowerType.Mana
+
+function DruidMana:UNIT_POWER_UPDATE()
+	local Mana = UnitPower("player", ManaID)
+	local MaxMana = UnitPowerMax("player", ManaID)
+	
+	self.Bar:SetValue(Mana)
+	self.Bar:SetMinMaxValues(0, MaxMana)
+	
+	self.Progress:SetText(format("%s / %s", vUI:ShortValue(Mana), vUI:ShortValue(MaxMana)))
+	self.Percentage:SetText(floor((Mana / MaxMana * 100 + 0.05) * 10) / 10 .. "%")
+end
+
+function DruidMana:UNIT_POWER_FREQUENT()
+	local Mana = UnitPower("player", ManaID)
+	local MaxMana = UnitPowerMax("player", ManaID)
+	
+	self.Bar:SetValue(Mana)
+	
+	self.Progress:SetText(format("%s / %s", vUI:ShortValue(Mana), vUI:ShortValue(MaxMana)))
+	self.Percentage:SetText(floor((Mana / MaxMana * 100 + 0.05) * 10) / 10 .. "%")
+end
+
+function DruidMana:PLAYER_ENTERING_WORLD()
+	self:SetScaledSize(Settings["unitframes-player-width"], Settings["unitframes-player-power-height"] + 2)
+	self:SetScaledPoint("CENTER", UIParent, 0, -100)
+	
+	self.Fade = CreateAnimationGroup(self)
+	
+	self.FadeIn = self.Fade:CreateAnimation("Fade")
+	self.FadeIn:SetEasing("in")
+	self.FadeIn:SetDuration(0.15)
+	self.FadeIn:SetChange(1)
+	
+	self.FadeOut = self.Fade:CreateAnimation("Fade")
+	self.FadeOut:SetEasing("out")
+	self.FadeOut:SetDuration(0.15)
+	self.FadeOut:SetChange(0)
+	self.FadeOut:SetScript("OnFinished", FadeOnFinished)
+	
+	self.BarBG = CreateFrame("Frame", nil, self)
+	self.BarBG:SetScaledPoint("TOPLEFT", self, 0, 0)
+	self.BarBG:SetScaledPoint("BOTTOMRIGHT", self, 0, 0)
+	self.BarBG:SetBackdrop(vUI.BackdropAndBorder)
+	self.BarBG:SetBackdropColor(0, 0, 0)
+	self.BarBG:SetBackdropBorderColor(0, 0, 0)
+	
+	self.Bar = CreateFrame("StatusBar", nil, self.BarBG)
+	self.Bar:SetStatusBarTexture(Media:GetTexture(Settings["ui-widget-texture"]))
+	self.Bar:SetScaledPoint("TOPLEFT", self.BarBG, 1, -1)
+	self.Bar:SetScaledPoint("BOTTOMRIGHT", self.BarBG, -1, 1)
+	self.Bar:SetFrameLevel(6)
+	
+	self.Bar:SetMinMaxValues(0, UnitPowerMax("player", ManaID))
+	self.Bar:SetStatusBarColorHex(Settings["color-mana"])
+	
+	self.Bar.BG = self.Bar:CreateTexture(nil, "BORDER")
+	self.Bar.BG:SetAllPoints(self.Bar)
+	self.Bar.BG:SetTexture(Media:GetTexture(Settings["ui-widget-texture"]))
+	self.Bar.BG:SetVertexColorHex(Settings["color-mana"])
+	self.Bar.BG:SetAlpha(0.2)
+	
+	self.Percentage = self.Bar:CreateFontString(nil, "OVERLAY")
+	self.Percentage:SetScaledPoint("LEFT", self.Bar, 3, 0)
+	self.Percentage:SetFontInfo(Settings["ui-widget-font"], Settings["ui-font-size"])
+	self.Percentage:SetJustifyH("LEFT")
+	
+	self.Progress = self.Bar:CreateFontString(nil, "OVERLAY")
+	self.Progress:SetScaledPoint("RIGHT", self.Bar, -3, 0)
+	self.Progress:SetFontInfo(Settings["ui-widget-font"], Settings["ui-font-size"])
+	self.Progress:SetJustifyH("RIGHT")
+	
+	vUI:GetModule("Move"):Add(self)
+	
+	self:UNIT_POWER_UPDATE()
+	--self:UPDATE_SHAPESHIFT_FORM()
+	
+	self:UnregisterEvent("PLAYER_ENTERING_WORLD")
+end
+
+function DruidMana:UPDATE_SHAPESHIFT_FORM()
+	if (GetShapeshiftForm() == 0) then
+		self:Hide()
+	else
+		self:Show()
+	end
+end
+
+function DruidMana:OnEvent(event)
+	if self[event] then
+		self[event](self)
+	end
+end
+
+DruidMana:RegisterEvent("UNIT_POWER_UPDATE")
+DruidMana:RegisterEvent("UNIT_POWER_FREQUENT")
+DruidMana:RegisterEvent("PLAYER_ENTERING_WORLD")
+--DruidMana:RegisterEvent("UPDATE_SHAPESHIFT_FORM")
+DruidMana:SetScript("OnEvent", DruidMana.OnEvent)
---
+++ vUI/Elements/MirrorTimers.lua
@@ -0,0 +1,111 @@
+local vUI, GUI, Language, Media, Settings, Defaults = select(2, ...):get()
+
+local MirrorTimers = vUI:NewModule("Mirror Timers")
+
+local GetMirrorTimerProgress = GetMirrorTimerProgress
+local GetMirrorTimerInfo = GetMirrorTimerInfo
+
+MirrorTimers.Colors = {
+	["EXHAUSTION"] = "FFE500",
+	["BREATH"] = "007FFF",
+	["DEATH"] = "FFB200",
+	["FEIGNDEATH"] = "FFB200",
+}
+
+function MirrorTimers:OnUpdate(self)
+	if self.Paused then
+		return
+	end
+	
+	self.Value = GetMirrorTimerProgress(self.Timer) / 1000
+	
+	if (self.Value > 0) then
+		self.Text:SetText(format("%s (%s)", self.Label, vUI:FormatTime(self.Value)))
+	else
+		self.Text:SetText(format("%s", self.Label))
+	end
+	
+	self:SetValue(self.Value)
+end
+
+function MirrorTimers:MIRROR_TIMER_PAUSE(ispaused)
+	if ispaused then
+		self.Bar.Paused = true
+	else
+		self.Bar.Paused = false
+	end
+end
+
+function MirrorTimers:MIRROR_TIMER_STOP()
+	self.Bar:Hide()
+end
+
+MirrorTimers.MirrorTimer_Show = function(timer, value, maxvalue, scale, paused, label)
+	MirrorTimers.Bar.Max = maxvalue
+	MirrorTimers.Bar.Value = value
+	MirrorTimers.Bar.Timer = timer
+	MirrorTimers.Bar.Label = label
+	
+	MirrorTimers.Bar:SetMinMaxValues(0, maxvalue / 1000)
+	MirrorTimers.Bar:SetValue(value)
+	MirrorTimers.Bar:SetStatusBarColorHex(MirrorTimers.Colors[timer])
+	MirrorTimers.BarBG:SetVertexColorHex(MirrorTimers.Colors[timer])
+	MirrorTimers.Bar.Text:SetText(format("%s (%s)", label, vUI:FormatTime(value / 1000)))
+	MirrorTimers.Bar:Show()
+end
+
+function MirrorTimers:Load()
+	self.Bar = CreateFrame("StatusBar", "vUI Timers Bar", UIParent)
+	self.Bar:SetScaledPoint("TOP", UIParent, 0, -120)
+	self.Bar:SetScaledSize(210, 20)
+	self.Bar:SetFrameLevel(5)
+	self.Bar:SetStatusBarTexture(Media:GetTexture(Settings["ui-widget-texture"]))
+	self.Bar:SetScript("OnUpdate", self.OnUpdate)
+	self.Bar:Hide()
+	
+	self.BarBG = self.Bar:CreateTexture(nil, "BORDER")
+	self.BarBG:SetScaledPoint("TOPLEFT", self.Bar, 0, 0)
+	self.BarBG:SetScaledPoint("BOTTOMRIGHT", self.Bar, 0, 0)
+	self.BarBG:SetTexture(Media:GetTexture(Settings["ui-widget-texture"]))
+	self.BarBG:SetAlpha(0.2)
+	
+	self.Bar.Text = self.Bar:CreateFontString(nil, "OVERLAY")
+	self.Bar.Text:SetScaledPoint("CENTER", self.Bar, 0, 0)
+	self.Bar.Text:SetFontInfo(Settings["ui-widget-font"], Settings["ui-font-size"])
+	self.Bar.Text:SetJustifyH("CENTER")
+	
+	self.BarOutline = self.Bar:CreateTexture(nil, "BORDER")
+	self.BarOutline:SetScaledPoint("TOPLEFT", self.Bar, -1, 1)
+	self.BarOutline:SetScaledPoint("BOTTOMRIGHT", self.Bar, 1, -1)
+	self.BarOutline:SetTexture(Media:GetTexture("Blank"))
+	self.BarOutline:SetVertexColor(0, 0, 0)
+	
+	self.OuterBG = CreateFrame("Frame", nil, self.Bar)
+	self.OuterBG:SetScaledPoint("TOPLEFT", self.Bar, -4, 4)
+	self.OuterBG:SetScaledPoint("BOTTOMRIGHT", self.Bar, 4, -4)
+	self.OuterBG:SetBackdrop(vUI.BackdropAndBorder)
+	self.OuterBG:SetBackdropBorderColor(0, 0, 0)
+	self.OuterBG:SetFrameLevel(1)
+	self.OuterBG:SetFrameStrata("BACKGROUND")
+	self.OuterBG:SetBackdropColorHex(Settings["ui-window-bg-color"])
+	
+	self:Hook("MirrorTimer_Show")
+	
+	self:RegisterEvent("MIRROR_TIMER_PAUSE")
+	self:RegisterEvent("MIRROR_TIMER_STOP")
+	
+	self.Hider = CreateFrame("Frame", nil, UIParent)
+	self.Hider:Hide()
+	
+	vUI:GetModule("Move"):Add(self.Bar, 6)
+	
+	for i = 1, MIRRORTIMER_NUMTIMERS do
+		_G["MirrorTimer" .. i]:SetParent(self.Hider)
+	end
+end
+
+MirrorTimers:SetScript("OnEvent", function(self, event, ...)
+	if self[event] then
+		self[event](self, ...)
+	end
+end)
