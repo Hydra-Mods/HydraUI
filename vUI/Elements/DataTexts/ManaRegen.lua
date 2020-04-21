c7a0c6e1e3a360af054331410b2392b5c5545c6c
Hydra <ryanandrews135@gmail.com>
2020-04-21 17:45:53 -0600

    Mana regen tooltip

--- vUI/Elements/DataTexts/ManaRegen.lua
+++ vUI/Elements/DataTexts/ManaRegen.lua
@@ -4,6 +4,20 @@ local GetManaRegen = GetManaRegen
 local InCombatLockdown = InCombatLockdown
 local Label = Language["Regen"]
 
+local OnEnter = function(self)
+	GameTooltip_SetDefaultAnchor(GameTooltip, self)
+	
+	local Base, Casting = GetManaRegen()
+	
+	GameTooltip:AddLine(format(MANA_COMBAT_REGEN_TOOLTIP, floor(Casting * 5)), nil, nil, nil, true)
+	
+	GameTooltip:Show()
+end
+
+local OnLeave = function()
+	GameTooltip:Hide()
+end
+
 local OnMouseUp = function()
 	ToggleCharacter("PaperDollFrame")
 end
@@ -14,22 +28,18 @@ local Update = function(self, event, unit)
 	end
 	
 	local Base, Casting = GetManaRegen()
-	local Regen
 	
-	if InCombatLockdown() then
-		Regen = Casting * 5
-	else
-		Regen = Base * 5
-	end
-	
-	self.Text:SetFormattedText("|cFF%s%s:|r |cFF%s%.0f|r", Settings["data-text-label-color"], Label, Settings["data-text-value-color"], Regen)
+	self.Text:SetFormattedText("|cFF%s%s:|r |cFF%s%.0f|r", Settings["data-text-label-color"], Label, Settings["data-text-value-color"], (InCombatLockdown() and Casting or Base) * 5)
 end
 
 local OnEnable = function(self)
 	self:RegisterEvent("PLAYER_REGEN_ENABLED")
 	self:RegisterEvent("PLAYER_REGEN_DISABLED")
+	self:RegisterEvent("UNIT_MAXPOWER")
 	self:SetScript("OnEvent", Update)
 	self:SetScript("OnMouseUp", OnMouseUp)
+	self:SetScript("OnEnter", OnEnter)
+	self:SetScript("OnLeave", OnLeave)
 	
 	self:Update("player")
 end
@@ -37,8 +47,11 @@ end
 local OnDisable = function(self)
 	self:UnregisterEvent("PLAYER_REGEN_ENABLED")
 	self:UnregisterEvent("PLAYER_REGEN_DISABLED")
+	self:UnregisterEvent("UNIT_MAXPOWER")
 	self:SetScript("OnEvent", nil)
 	self:SetScript("OnMouseUp", nil)
+	self:SetScript("OnEnter", nil)
+	self:SetScript("OnLeave", nil)
 	
 	self.Text:SetText("")
 end
