a20f6a4618daa3592fd96b969a91508c1054f9e0
Hydra <ryanandrews135@gmail.com>
2020-05-31 17:37:39 -0600

    This doesn't need a new value

--- vUI/Elements/General.lua
+++ vUI/Elements/General.lua
@@ -99,13 +99,11 @@ local AcceptNewStyle = function(value)
 end
 
 local UpdateStyle = function(value)
-	local Label = value
-	
 	if Assets.Styles[value]["ui-widget-color"] then
-		Label = format("|cFF%s%s|r", Assets.Styles[value]["ui-widget-color"], value)
+		value = format("|cFF%s%s|r", Assets.Styles[value]["ui-widget-color"], value)
 	end
 	
-	vUI:DisplayPopup(Language["Attention"], format(Language['Are you sure you would like to change to the current style to "%s"?'], Label), Language["Accept"], AcceptNewStyle, Language["Cancel"], nil, value)
+	vUI:DisplayPopup(Language["Attention"], format(Language['Are you sure you would like to change to the current style to "%s"?'], value), Language["Accept"], AcceptNewStyle, Language["Cancel"], nil, value)
 end
 
 GUI:AddOptions(function(self)
