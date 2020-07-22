5ee3e6316e82384e127b2305d53ef416c32676eb
Hydra <ryanandrews135@gmail.com>
2020-07-21 22:59:44 -0600

    Small optimization

--- vUI/Elements/Minimap/MinimapButtons.lua
+++ vUI/Elements/Minimap/MinimapButtons.lua
@@ -71,8 +71,10 @@ local RemoveByID = {
 }
 
 local IsIgnoredAddOn = function(name)
+	name = lower(name)
+
 	for i = 1, #IgnoredAddOns do
-		if find(lower(name), IgnoredAddOns[i]) then
+		if find(name, IgnoredAddOns[i]) then
 			return true
 		end
 	end
