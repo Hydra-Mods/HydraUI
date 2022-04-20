3564208f564f0677aeacba5e558f449481985f01
Hydra <ryanandrews135@gmail.com>
2022-04-19 22:44:13 -0600

    Put all the backdrops together

--- Elements/Tools.lua
+++ Elements/Tools.lua
@@ -228,14 +228,32 @@ function HydraUI:SetFontInfo(object, font, size, flags)
 	end
 end
 
+-- Backdrops
+HydraUI.Backdrop = {
+	bgFile = "Interface\\AddOns\\HydraUI\\Assets\\Textures\\HydraUIBlank.tga",
+	insets = {top = 0, left = 0, bottom = 0, right = 0},
+}
+
+HydraUI.BackdropAndBorder = {
+	bgFile = "Interface\\AddOns\\HydraUI\\Assets\\Textures\\HydraUIBlank.tga",
+	edgeFile = "Interface\\AddOns\\HydraUI\\Assets\\Textures\\HydraUIBlank.tga",
+	edgeSize = 1,
+	insets = {top = 0, left = 0, bottom = 0, right = 0},
+}
+
+HydraUI.Outline = {
+	edgeFile = "Interface\\AddOns\\HydraUI\\Assets\\Textures\\HydraUIBlank.tga",
+	edgeSize = 1,
+	insets = {left = 0, right = 0, top = 0, bottom = 0},
+}
+
 local Outside = {
-	bgFile = Assets:GetTexture("Blank"),
-	edgeFile = Assets:GetTexture("Blank"),
+	edgeFile = "Interface\\AddOns\\HydraUI\\Assets\\Textures\\HydraUIBlank.tga",
 }
 
 local Inside = {
-	bgFile = Assets:GetTexture("Blank"),
-	edgeFile = Assets:GetTexture("Blank"),
+	bgFile = "Interface\\AddOns\\HydraUI\\Assets\\Textures\\HydraUIBlank.tga",
+	edgeFile = "Interface\\AddOns\\HydraUI\\Assets\\Textures\\HydraUIBlank.tga",
 }
 
 function HydraUI:AddBackdrop(frame, texture)
--- Initialize.lua
+++ Initialize.lua
@@ -33,25 +33,6 @@ if (HydraUI.UserLocale == "enGB") then
 	HydraUI.UserLocale = "enUS"
 end
 
--- Backdrops
-HydraUI.Backdrop = {
-	bgFile = "Interface\\AddOns\\HydraUI\\Assets\\Textures\\HydraUIBlank.tga",
-	insets = {top = 0, left = 0, bottom = 0, right = 0},
-}
-
-HydraUI.BackdropAndBorder = {
-	bgFile = "Interface\\AddOns\\HydraUI\\Assets\\Textures\\HydraUIBlank.tga",
-	edgeFile = "Interface\\AddOns\\HydraUI\\Assets\\Textures\\HydraUIBlank.tga",
-	edgeSize = 1,
-	insets = {top = 0, left = 0, bottom = 0, right = 0},
-}
-
-HydraUI.Outline = {
-	edgeFile = "Interface\\AddOns\\HydraUI\\Assets\\Textures\\HydraUIBlank.tga",
-	edgeSize = 1,
-	insets = {left = 0, right = 0, top = 0, bottom = 0},
-}
-
 -- Language
 local Language = {}
 
