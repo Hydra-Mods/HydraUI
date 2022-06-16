05ebdb3b283281bd58a1d4052b66bea179b06bb9
Hydra <ryanandrews135@gmail.com>
2022-06-15 22:50:23 -0600

    rewrite zone text
    Old version was a Blizzard clone, rewrite it at half the size in modern fashion

--- Elements/ZoneText.lua
+++ Elements/ZoneText.lua
@@ -1,247 +1,108 @@
 local HydraUI, Language, Assets, Settings = select(2, ...):get()
 
-local ZoneTextSize = 32
-local SubZoneTextSize = 26
-local AutoFollowTextSize = 20
+local ZT = HydraUI:NewModule("Zone Text")
 
-local GetZonePVPInfo = GetZonePVPInfo
-local GetSubZoneText = GetSubZoneText
 local GetZoneText = GetZoneText
+local GetSubZoneText = GetSubZoneText
+local GetZonePVPInfo = GetZonePVPInfo
+local SANCTUARY_TERRITORY = SANCTUARY_TERRITORY
+local FREE_FOR_ALL_TERRITORY = FREE_FOR_ALL_TERRITORY
+local FACTION_CONTROLLED_TERRITORY = FACTION_CONTROLLED_TERRITORY
+local CONTESTED_TERRITORY = CONTESTED_TERRITORY
+local COMBAT_ZONE = COMBAT_ZONE
 
-local FADE_IN_TIME = 0.4
-local FADE_OUT_TIME = 1.4
-local HOLD_TIME = 1.6
+local PVPText = {
+	sanctuary = SANCTUARY_TERRITORY,
+	arena = FREE_FOR_ALL_TERRITORY,
+	contested = CONTESTED_TERRITORY,
+	combat = COMBAT_ZONE,
+}
 
-local CustomZoneText = HydraUI:NewModule("Zone Text")
+local PVPTextFormat = {
+	friendly = 1,
+	hostile = 1,
+}
 
-local CustomZoneTextFrame = CreateFrame("Frame", nil, HydraUI.UIParent)
-CustomZoneTextFrame:SetSize(200, ZoneTextSize)
-CustomZoneTextFrame:SetPoint("TOP", HydraUI.UIParent, 0, -220)
-CustomZoneTextFrame:SetAlpha(0)
-
-CustomZoneTextFrame.Group = CreateAnimationGroup(CustomZoneTextFrame)
-
-CustomZoneTextFrame.FadeIn = CustomZoneTextFrame.Group:CreateAnimation("fade")
-CustomZoneTextFrame.FadeIn:SetEasing("in")
-CustomZoneTextFrame.FadeIn:SetDuration(FADE_IN_TIME)
-CustomZoneTextFrame.FadeIn:SetChange(1)
-CustomZoneTextFrame.FadeIn:SetScript("OnPlay", function(self) self.Parent:Show() end)
-
-CustomZoneTextFrame.Sleep = CustomZoneTextFrame.Group:CreateAnimation("sleep")
-CustomZoneTextFrame.Sleep:SetDuration(HOLD_TIME)
-CustomZoneTextFrame.Sleep:SetOrder(2)
-
-CustomZoneTextFrame.FadeOut = CustomZoneTextFrame.Group:CreateAnimation("fade")
-CustomZoneTextFrame.FadeOut:SetEasing("out")
-CustomZoneTextFrame.FadeOut:SetDuration(FADE_OUT_TIME)
-CustomZoneTextFrame.FadeOut:SetChange(0)
-CustomZoneTextFrame.FadeOut:SetOrder(3)
-CustomZoneTextFrame.FadeOut:SetScript("OnFinished", function(self) self.Parent:Hide() end)
-
-local CustomSubZoneTextFrame = CreateFrame("Frame", nil, HydraUI.UIParent)
-CustomSubZoneTextFrame:SetSize(200, SubZoneTextSize)
-CustomSubZoneTextFrame:SetPoint("TOP", CustomZoneTextFrame, "BOTTOM", 0, 0)
-CustomSubZoneTextFrame:SetAlpha(0)
-
-CustomSubZoneTextFrame.Group = CreateAnimationGroup(CustomSubZoneTextFrame)
-
-CustomSubZoneTextFrame.FadeIn = CustomSubZoneTextFrame.Group:CreateAnimation("fade")
-CustomSubZoneTextFrame.FadeIn:SetEasing("in")
-CustomSubZoneTextFrame.FadeIn:SetDuration(FADE_IN_TIME)
-CustomSubZoneTextFrame.FadeIn:SetChange(1)
-CustomSubZoneTextFrame.FadeIn:SetScript("OnPlay", function(self) self.Parent:Show() end)
-
-CustomSubZoneTextFrame.Sleep = CustomSubZoneTextFrame.Group:CreateAnimation("sleep")
-CustomSubZoneTextFrame.Sleep:SetDuration(HOLD_TIME)
-CustomSubZoneTextFrame.Sleep:SetOrder(2)
-
-CustomSubZoneTextFrame.FadeOut = CustomSubZoneTextFrame.Group:CreateAnimation("fade")
-CustomSubZoneTextFrame.FadeOut:SetEasing("out")
-CustomSubZoneTextFrame.FadeOut:SetDuration(FADE_OUT_TIME)
-CustomSubZoneTextFrame.FadeOut:SetChange(0)
-CustomSubZoneTextFrame.FadeOut:SetOrder(3)
-CustomSubZoneTextFrame.FadeOut:SetScript("OnFinished", function(self) self.Parent:Hide() end)
-
-local ZoneText = CustomZoneTextFrame:CreateFontString(nil, "OVERLAY")
-ZoneText:SetPoint("CENTER", CustomZoneTextFrame, 0, 0)
-ZoneText:SetShadowColor(0, 0, 0)
-ZoneText:SetShadowOffset(1, -1)
-
-local PVPInfoText = CustomSubZoneTextFrame:CreateFontString(nil, "OVERLAY")
-PVPInfoText:SetPoint("CENTER", CustomSubZoneTextFrame, 0, 0)
-PVPInfoText:SetShadowColor(0, 0, 0)
-PVPInfoText:SetShadowOffset(1, -1)
-
-local SubZoneText = CustomSubZoneTextFrame:CreateFontString(nil, "OVERLAY")
-SubZoneText:SetPoint("CENTER", CustomSubZoneTextFrame, 0, 0)
-SubZoneText:SetShadowColor(0, 0, 0)
-SubZoneText:SetShadowOffset(1, -1)
-
-local PVPArenaText = CustomSubZoneTextFrame:CreateFontString(nil, "OVERLAY")
-PVPArenaText:SetPoint("CENTER", CustomSubZoneTextFrame, 0, 0)
-PVPArenaText:SetShadowColor(0, 0, 0)
-PVPArenaText:SetShadowOffset(1, -1)
-
-local SetZoneText = function(show)
+function ZT:OnEvent(event)
+	local Zone = GetZoneText()
+	local SubZone = GetSubZoneText()
 	local PVPType, IsSubZonePVP, Faction = GetZonePVPInfo()
+	local Color = HydraUI.ZoneColors[PVPType or "other"]
 	
-	if (not ZoneText) then
-		return
-	end
+	self.ZoneText:SetText("")
+	self.SubZoneText:SetText("")
+	self.PVPText:SetText("")
 	
-	PVPArenaText:SetText("")
-	PVPInfoText:SetText("")
+	self.ZoneText:SetTextColor(Color[1], Color[2], Color[3])
+	self.SubZoneText:SetTextColor(Color[1], Color[2], Color[3])
+	self.PVPText:SetTextColor(Color[1], Color[2], Color[3])
 	
-	local PVPText = PVPInfoText
-	
-	if IsSubZonePVP then
-		PVPText = PVPArenaText
-	end
-	
-	if (not PVPType) then
-		PVPType = "other"
-	end
-	
-	local Color = HydraUI.ZoneColors[PVPType]
-	
-	if (PVPType == "sanctuary") then
-		PVPText:SetText(SANCTUARY_TERRITORY)
-		PVPText:SetTextColor(Color[1], Color[2], Color[3])
-		ZoneText:SetTextColor(Color[1], Color[2], Color[3])
-		SubZoneText:SetTextColor(Color[1], Color[2], Color[3])
-	elseif (PVPType == "arena") then
-		PVPText:SetText(FREE_FOR_ALL_TERRITORY)
-		PVPText:SetTextColor(Color[1], Color[2], Color[3])
-		ZoneText:SetTextColor(Color[1], Color[2], Color[3])
-		SubZoneText:SetTextColor(Color[1], Color[2], Color[3])
-	elseif (PVPType == "friendly") then
-		if (Faction and Faction ~= "") then
-			PVPText:SetFormattedText(FACTION_CONTROLLED_TERRITORY, Faction)
-			PVPText:SetTextColor(Color[1], Color[2], Color[3])
+	if (event == "ZONE_CHANGED_NEW_AREA" or Zone ~= self.CurrentZone) and (PVPType and PVPType ~= "") then
+		if PVPText[PVPType] then
+			self.PVPText:SetText(PVPText[PVPType])
+		elseif (PVPTextFormat[PVPType] and Faction and Faction ~= "") then
+			self.PVPText:SetFormattedText(FACTION_CONTROLLED_TERRITORY, Faction)
 		end
-		
-		ZoneText:SetTextColor(Color[1], Color[2], Color[3])
-		SubZoneText:SetTextColor(Color[1], Color[2], Color[3])
-	elseif (PVPType == "hostile") then
-		if (Faction and Faction ~= "") then
-			PVPText:SetFormattedText(FACTION_CONTROLLED_TERRITORY, Faction)
-			PVPText:SetTextColor(Color[1], Color[2], Color[3])
-		end
-		
-		ZoneText:SetTextColor(Color[1], Color[2], Color[3])
-		SubZoneText:SetTextColor(Color[1], Color[2], Color[3])
-	elseif (PVPType == "contested") then
-		PVPText:SetText(CONTESTED_TERRITORY)
-		PVPText:SetTextColor(Color[1], Color[2], Color[3])
-		ZoneText:SetTextColor(Color[1], Color[2], Color[3])
-		SubZoneText:SetTextColor(Color[1], Color[2], Color[3])
-	elseif (PVPType == "combat") then
-		PVPText = PVPArenaTextString
-		PVPText:SetText(COMBAT_ZONE)
-		PVPText:SetTextColor(Color[1], Color[2], Color[3])
-		ZoneText:SetTextColor(Color[1], Color[2], Color[3])
-		SubZoneText:SetTextColor(Color[1], Color[2], Color[3])
-	elseif (PVPType == "other") then
-		ZoneText:SetTextColor(Color[1], Color[2], Color[3])
-		SubZoneText:SetTextColor(Color[1], Color[2], Color[3])
 	end
 	
-	SubZoneText:ClearAllPoints()
-	
-	if (ZonePVPType ~= PVPType) then
-		ZonePVPType = PVPType
-	elseif (not show) then
-		PVPInfoText:SetText("")
-		SubZoneText:SetPoint("CENTER", CustomSubZoneTextFrame, 0, 0)
+	if (Zone and Zone ~= self.CurrentZone or SubZone == "") then
+		self.ZoneText:SetText(Zone)
+		self.CurrentZone = Zone
 	end
 	
-	if (PVPInfoText:GetText() == "") then
-		SubZoneText:SetPoint("CENTER", CustomSubZoneTextFrame, 0, 0)
-	else
-		SubZoneText:SetPoint("TOP", PVPInfoText, "BOTTOM", 0, -14)
+	self.SubZoneText:SetText(SubZone)
+	
+	if self.Group:IsPlaying() then
+		self.Group:Stop()
 	end
+	
+	self.Group:Play()
 end
 
-local OnEvent = function(self, event)
-	local ShowZoneText = false
-	local ZoneString = GetZoneText()
+function ZT:CreateFontObjects()
+	local ZoneFrame = CreateFrame("Frame", nil, HydraUI.UIParent)
+	ZoneFrame:SetSize(32, 32)
+	ZoneFrame:SetPoint("TOP", HydraUI.UIParent, 0, -320) -- SetPoint("TOP", HydraUI.UIParent, 0, -220)
+	ZoneFrame:SetAlpha(0)
+	-- Make a mover too? :thinking:
 	
-	if ((ZoneString ~= self.ZoneText) or (event == "ZONE_CHANGED_NEW_AREA")) then
-		self.ZoneText = ZoneString
-		ZoneText:SetText(ZoneString)
-		ShowZoneText = true
-		SetZoneText(ShowZoneText)
-
-		if CustomZoneTextFrame.Group:IsPlaying() then
-			CustomZoneTextFrame.Group:Stop()
-			CustomZoneTextFrame:SetAlpha(1)
-			CustomZoneTextFrame.Sleep:Play()
-		else
-			CustomZoneTextFrame.Group:Play()
-		end
-	end
+	local ZoneText = ZoneFrame:CreateFontString(nil, "OVERLAY")
+	ZoneText:SetFont(Assets:GetFont(Settings["ui-widget-font"]), 32, "OUTLINE")
+	ZoneText:SetPoint("CENTER", ZoneFrame, 0, 0)
 	
-	local SubzoneString = GetSubZoneText()
+	local SubZoneText = ZoneFrame:CreateFontString(nil, "OVERLAY")
+	SubZoneText:SetFont(Assets:GetFont(Settings["ui-widget-font"]), 26, "OUTLINE")
+	SubZoneText:SetPoint("TOP", ZoneText, "BOTTOM", 0, 0)
 	
-	if (SubzoneString == "" and not ShowZoneText) then
-		SubzoneString = ZoneString
-	end
+	local PVPText = ZoneFrame:CreateFontString(nil, "OVERLAY")
+	PVPText:SetFont(Assets:GetFont(Settings["ui-widget-font"]), 26, "OUTLINE")
+	PVPText:SetPoint("TOP", SubZoneText, "BOTTOM", 0, 0)
 	
-	SubZoneText:SetText("")
+	local Group = CreateAnimationGroup(ZoneFrame)
 	
-	if (SubzoneString == ZoneString) then
-		ShowZoneText = false
-		
-		if (not CustomZoneTextFrame:IsShown()) then
-			SubZoneText:SetText(SubzoneString)
-			SetZoneText(ShowZoneText)
-			
-			if CustomSubZoneTextFrame.Group:IsPlaying() then
-				CustomSubZoneTextFrame.Group:Stop()
-				CustomSubZoneTextFrame:SetAlpha(1)
-				CustomSubZoneTextFrame.Sleep:Play()
-			else
-				CustomSubZoneTextFrame.Group:Play()
-			end
-		end
-	else
-		if CustomZoneTextFrame:IsShown() then
-			ShowZoneText = true
-		end
-		
-		SubZoneText:SetText(SubzoneString)
-		SetZoneText(ShowZoneText)
-		
-		if CustomSubZoneTextFrame.Group:IsPlaying() then
-			CustomSubZoneTextFrame.Group:Stop()
-			CustomSubZoneTextFrame:SetAlpha(1)
-			CustomSubZoneTextFrame.Sleep:Play()
-		else
-			CustomSubZoneTextFrame.Group:Play()
-		end
-	end
+	local ZoneFadeIn = Group:CreateAnimation("fade")
+	ZoneFadeIn:SetEasing("in")
+	ZoneFadeIn:SetDuration(0.4)
+	ZoneFadeIn:SetOrder(1)
+	ZoneFadeIn:SetChange(1)
+	
+	local ZoneSleep = Group:CreateAnimation("sleep")
+	ZoneSleep:SetDuration(2.5)
+	ZoneSleep:SetOrder(2)
+	
+	local ZoneFadeOut = Group:CreateAnimation("fade")
+	ZoneFadeOut:SetEasing("out")
+	ZoneFadeOut:SetDuration(0.75)
+	ZoneFadeOut:SetChange(0)
+	ZoneFadeOut:SetOrder(3)
+	
+	self.ZoneText = ZoneText
+	self.SubZoneText = SubZoneText
+	self.PVPText = PVPText
+	self.Group = Group
 end
 
-CustomZoneText["ZONE_CHANGED"] = OnEvent
-CustomZoneText["ZONE_CHANGED_INDOORS"] = OnEvent
-CustomZoneText["ZONE_CHANGED_NEW_AREA"] = OnEvent
-
-local UpdateZoneTextFont = function(value)
-	--ZoneText:SetFont(Assets:GetFont(Settings["ui-header-font"]), 32)
-end
-
-function CustomZoneText:OnEvent(event, arg)
-	if self[event] then
-		self[event](self, event, arg)
-	end
-end
-
-function CustomZoneText:Load()
-	ZoneText:SetFont(Assets:GetFont(Settings["ui-header-font"]), ZoneTextSize)
-	PVPInfoText:SetFont(Assets:GetFont(Settings["ui-header-font"]), SubZoneTextSize)
-	SubZoneText:SetFont(Assets:GetFont(Settings["ui-header-font"]), SubZoneTextSize)
-	PVPArenaText:SetFont(Assets:GetFont(Settings["ui-header-font"]), SubZoneTextSize)
-	
+function ZT:Load()
 	-- Kill default zone texts
 	if ZoneTextFrame then
 		ZoneTextFrame:UnregisterAllEvents()
@@ -249,17 +110,12 @@ function CustomZoneText:Load()
 		ZoneTextFrame:SetScript("OnUpdate", nil)
 	end
 	
-	SetZoneText()
+	self:CreateFontObjects()
+	
+	self.CurrentZone = nil -- nil instead of GetZoneText() so the first OnEvent call shows pvp info
 	
 	self:RegisterEvent("ZONE_CHANGED")
 	self:RegisterEvent("ZONE_CHANGED_INDOORS")
 	self:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 	self:SetScript("OnEvent", self.OnEvent)
-end
-
-ZoneText_Clear = function()
-	ZoneText:SetText("")
-	PVPInfoText:SetText("")
-	SubZoneText:SetText("")
-	PVPArenaText:SetText("")
 end
