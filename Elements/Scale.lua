fb01e8e3d2c5191d0b0c3d7000398e864972c006
Hydra <ryanandrews135@gmail.com>
2022-09-17 12:48:07 -0600

    Add a default ui scale slider with popup

--- Elements/Scale.lua
+++ Elements/Scale.lua
@@ -5,10 +5,19 @@ local ScaleOnAccept = function()
 end
 
 local SetSuggestedScale = function()
-	HydraUI:DisplayPopup(Language["Attention"], format(Language["Are you sure you would like to change your UI scale to the suggested setting of %s?"], (768 / select(2, GetPhysicalScreenSize()))), ACCEPT, ScaleOnAccept, CANCEL)
+	HydraUI:DisplayPopup(Language["Attention"], format(Language["Are you sure you would like to change your UI scale to %s?"], (768 / select(2, GetPhysicalScreenSize()))), ACCEPT, ScaleOnAccept, CANCEL)
+end
+
+local UpdateScaleCVar = function(value)
+	C_CVar.SetCVar("uiScale", value)
+end
+
+local SetScaleFromSlider = function(value)
+	HydraUI:DisplayPopup(Language["Attention"], format(Language["Are you sure you would like to change your UI scale to %s?"], value), ACCEPT, UpdateScaleCVar, CANCEL, nil, value)
 end
 
 HydraUI:GetModule("GUI"):AddWidgets(Language["General"], Language["General"], function(left, right)
 	right:CreateHeader(Language["Scale"])
-	right:CreateButton("", Language["Apply"], Language["Set Suggested Scale"], Language["Apply the scale recommended based on your resolution"], SetSuggestedScale)
+	right:CreateButton("", Language["Apply"], Language["Set Suggested Scale"], Language["Apply the recommended scale based on your resolution"], SetSuggestedScale)
+	right:CreateSlider("ui-scale", C_CVar.GetCVar("uiScale"), 0.4, 1.2, 0.01, Language["Set UI Scale"], Language["Adjust the UI scale"], SetScaleFromSlider)
 end)
